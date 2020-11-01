9f350cff8404f9c13d3979548a093f69f792c45f
Dale Aguil <daleaguil@gmail.com>
2020-11-01 14:13:32 +0800

    Added Quotes Page

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new GreetPage();
+            MainPage = new QuotesPage();
         }
 
         protected override void OnStart()
