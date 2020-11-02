443e2ba97ed1fc5efe9127c0263d2ddc15bc6289
Dale Aguil <daleaguil@gmail.com>
2020-11-02 08:57:04 +0800

    Added 2 StackLayout exercises

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new StackPage();
+            MainPage = new StackLayoutExercise2();
         }
 
         protected override void OnStart()
