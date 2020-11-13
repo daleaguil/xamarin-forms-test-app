ca0e2a7c5193157f0f49c445450c4b97af1c9a31
Dale Aguil <daleaguil@gmail.com>
2020-11-13 15:04:39 +0800

    Added a TabbedPage

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new ContactsPage();
+            MainPage = new TabbedMainPage();
         }
 
         protected override void OnStart()
