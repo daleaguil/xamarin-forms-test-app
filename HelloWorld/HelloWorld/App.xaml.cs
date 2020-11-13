fcef2a94d85847ba2a26d29be3f5fc14098eef8c
Dale Aguil <daleaguil@gmail.com>
2020-11-13 18:24:58 +0800

    Added a ToolbarItem

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
